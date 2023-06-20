32cc489f5f7d3ebd53b672b1464802ed858acf5e
DavidOBr <david.obrien.nord@gmail.com>
2023-06-21 00:49:18 +0100

    created a homepage and meeting page and styled with bootstrap

---
+++ meetings/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+
+# Meeting views will be stored here.
--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homepage(request):
+    return render(request, 'website/homepage.html')
+
+
+def meeting(request):
+    return render(request, 'website/meetings.html')
